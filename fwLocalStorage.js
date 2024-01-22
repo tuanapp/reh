5ff2b494621f7bde5ebf330228473037c63f94c9
tuanapp <kdx.shaggy@gmail.com>
2024-01-22 19:37:28 +0530

    cached words

---
+++ fwLocalStorage.js
@@ -0,0 +1,26 @@
+let audioContext;
+function init() {
+    
+}
+init();
+
+function saveStringList(list, key, overwrite = false) {
+    if (!Array.isArray(list)) throw new TypeError('List must be an array.');
+    if (localStorage.getItem(key) && !overwrite) return;
+    localStorage.setItem(key, JSON.stringify(list));
+  }
+  
+  function getStringList(key) {
+    const storedList = localStorage.getItem(key);
+    if (!storedList) return [];
+    return JSON.parse(storedList);
+  }
+  
+  function doesStringListExist(key) {
+    return localStorage.getItem(key) !== null;
+  }
+  
+  function deleteStringList(key) {
+    localStorage.removeItem(key);
+  }
+  
